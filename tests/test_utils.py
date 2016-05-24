82712830f97183615a2a7f1441547a2e2871583b
roll <roll@post.agency>
2016-05-24 18:27:18 +0300

    fixed py2 test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import six
 import pytest
 from jsontableschema_pandas.utils import force_list
 
@@ -14,6 +15,6 @@ def test_force_list():
 
 
 def test_force_list_error():
-    with pytest.raises(TypeError) as excinfo:
+    with pytest.raises(TypeError) as exception:
         force_list('a', int)
-    assert str(excinfo.value) == "'a' is not one of list, int"
+        assert isinstance(exception, TypeError)
